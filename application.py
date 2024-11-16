d57069f7821c85c418e4daf0c966aa96b9d82888
Marc-Sahakian <marc.antonio.sahakian@gmail.com>
2024-11-16 13:52:31 +0100

    App

--- application.py
+++ application.py
@@ -1,7 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 13:10:21 2024
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
 
-@author: marca
-"""
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+application = Flask(__name__)
 
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    print("Home page accessed")
+    return render_template('index.html')
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run()        
